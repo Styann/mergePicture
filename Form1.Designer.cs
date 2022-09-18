5bd30536519774cdc51a07a8c621781aa2cdc1b0
boudiou <steunouyann44@gmail.com>
2022-09-18 20:33:47 +0200

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace mergePicture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.mergeBtn = new System.Windows.Forms.Button();
+            this.btnImage1 = new System.Windows.Forms.Button();
+            this.btnImage2 = new System.Windows.Forms.Button();
+            this.pictureBoxResult = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btnImage3 = new System.Windows.Forms.Button();
+            this.labelError = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.labelMaxImage = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.testLabel = new System.Windows.Forms.Label();
+            this.testButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResult)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(261, 158);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragDrop);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(6, 47);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(260, 158);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // mergeBtn
+            // 
+            this.mergeBtn.Location = new System.Drawing.Point(24, 333);
+            this.mergeBtn.Name = "mergeBtn";
+            this.mergeBtn.Size = new System.Drawing.Size(75, 23);
+            this.mergeBtn.TabIndex = 2;
+            this.mergeBtn.Text = "Merge";
+            this.mergeBtn.UseVisualStyleBackColor = true;
+            this.mergeBtn.Click += new System.EventHandler(this.mergeBtn_Click);
+            // 
+            // btnImage1
+            // 
+            this.btnImage1.Location = new System.Drawing.Point(6, 18);
+            this.btnImage1.Name = "btnImage1";
+            this.btnImage1.Size = new System.Drawing.Size(33, 23);
+            this.btnImage1.TabIndex = 3;
+            this.btnImage1.Text = "...";
+            this.btnImage1.UseVisualStyleBackColor = true;
+            this.btnImage1.Click += new System.EventHandler(this.btnImage1_Click);
+            // 
+            // btnImage2
+            // 
+            this.btnImage2.Location = new System.Drawing.Point(6, 18);
+            this.btnImage2.Name = "btnImage2";
+            this.btnImage2.Size = new System.Drawing.Size(33, 23);
+            this.btnImage2.TabIndex = 4;
+            this.btnImage2.Text = "...";
+            this.btnImage2.UseVisualStyleBackColor = true;
+            this.btnImage2.Click += new System.EventHandler(this.btnImage2_Click);
+            // 
+            // pictureBoxResult
+            // 
+            this.pictureBoxResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxResult.Location = new System.Drawing.Point(24, 362);
+            this.pictureBoxResult.Name = "pictureBoxResult";
+            this.pictureBoxResult.Size = new System.Drawing.Size(518, 158);
+            this.pictureBoxResult.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxResult.TabIndex = 5;
+            this.pictureBoxResult.TabStop = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(108, 333);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(467, 333);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 8;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(6, 47);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(261, 158);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // btnImage3
+            // 
+            this.btnImage3.Location = new System.Drawing.Point(6, 19);
+            this.btnImage3.Name = "btnImage3";
+            this.btnImage3.Size = new System.Drawing.Size(33, 23);
+            this.btnImage3.TabIndex = 10;
+            this.btnImage3.Text = "...";
+            this.btnImage3.UseVisualStyleBackColor = true;
+            this.btnImage3.Click += new System.EventHandler(this.btnImage3_Click);
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.BackColor = System.Drawing.SystemColors.Control;
+            this.labelError.ForeColor = System.Drawing.Color.Red;
+            this.labelError.Location = new System.Drawing.Point(209, 338);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(28, 13);
+            this.labelError.TabIndex = 11;
+            this.labelError.Text = "error";
+            this.labelError.Visible = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4"});
+            this.comboBox1.Location = new System.Drawing.Point(155, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 12;
+            // 
+            // labelMaxImage
+            // 
+            this.labelMaxImage.AutoSize = true;
+            this.labelMaxImage.Location = new System.Drawing.Point(38, 15);
+            this.labelMaxImage.Name = "labelMaxImage";
+            this.labelMaxImage.Size = new System.Drawing.Size(112, 13);
+            this.labelMaxImage.TabIndex = 13;
+            this.labelMaxImage.Text = "Select image number :";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.btnImage1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(284, 218);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pictureBox2);
+            this.groupBox2.Controls.Add(this.btnImage2);
+            this.groupBox2.Location = new System.Drawing.Point(302, 39);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(284, 218);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.pictureBox3);
+            this.groupBox3.Controls.Add(this.btnImage3);
+            this.groupBox3.Location = new System.Drawing.Point(592, 39);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(284, 218);
+            this.groupBox3.TabIndex = 15;
+            this.groupBox3.TabStop = false;
+            // 
+            // testLabel
+            // 
+            this.testLabel.AutoSize = true;
+            this.testLabel.Location = new System.Drawing.Point(755, 401);
+            this.testLabel.Name = "testLabel";
+            this.testLabel.Size = new System.Drawing.Size(35, 13);
+            this.testLabel.TabIndex = 16;
+            this.testLabel.Text = "label1";
+            // 
+            // testButton
+            // 
+            this.testButton.Location = new System.Drawing.Point(674, 396);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(75, 23);
+            this.testButton.TabIndex = 17;
+            this.testButton.Text = "button1";
+            this.testButton.UseVisualStyleBackColor = true;
+            this.testButton.Click += new System.EventHandler(this.testButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1011, 549);
+            this.Controls.Add(this.testButton);
+            this.Controls.Add(this.testLabel);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.labelMaxImage);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.labelError);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.pictureBoxResult);
+            this.Controls.Add(this.mergeBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResult)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button mergeBtn;
+        private System.Windows.Forms.Button btnImage1;
+        private System.Windows.Forms.Button btnImage2;
+        private System.Windows.Forms.PictureBox pictureBoxResult;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnImage3;
+        private System.Windows.Forms.Label labelError;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label labelMaxImage;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label testLabel;
+        private System.Windows.Forms.Button testButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mergePicture
+{
+    public partial class Form1 : Form
+    {
+        private List<Bitmap> bms;
+        private Bitmap result;
+        private string filter;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.filter = "Image Files(*.BMP;*.JPG;*.PNG;)|*.BMP;*.JPG;*.PNG|All files (*.*)|*.*";
+            this.bms = new List<Bitmap>();
+
+            pictureBox1.AllowDrop = true;
+            pictureBox2.AllowDrop = true;
+            pictureBox3.AllowDrop = true;
+
+            test();
+        }
+
+        private void test()
+        {
+            if (this.bms.Count() == 0) btnImage2.Enabled = false;
+            else btnImage2.Enabled = true;
+            if (this.bms.Count() == 1) btnImage2.Enabled = false;
+            else btnImage2.Enabled = true;
+            if (this.bms.Count() == 2) btnImage2.Enabled = false;
+            else btnImage2.Enabled = true;
+        }
+
+        private void btnImage1_Click(object sender, EventArgs e)
+        {
+            Bitmap bm = openImage(pictureBox1);
+            if(bm == null && this.bms.Count() > 0)
+            {
+                this.bms.Remove(this.bms[0]);
+            }
+            else
+            {
+                if (this.bms.Count() == 0) this.bms.Add(bm);
+                else this.bms[0] = bm;
+            }
+            btnImage2.Enabled = true;
+        }
+
+
+        private void btnImage2_Click(object sender, EventArgs e)
+        {
+            Bitmap bm = openImage(pictureBox2);
+            if (bm == null && this.bms.Count() > 1)
+            {
+                this.bms.Remove(this.bms[1]);
+            }
+            else
+            {
+                if (this.bms.Count() == 1) this.bms.Add(bm);
+                else this.bms[1] = bm;
+            }
+            //btnImage3.Enabled = true;
+        }
+
+
+        private void btnImage3_Click(object sender, EventArgs e)
+        {
+            Bitmap bm = openImage(pictureBox3);
+            if (bm == null && this.bms.Count() > 2) this.bms.Remove(this.bms[2]);
+            else
+            {
+                if(this.bms.Count() == 2) this.bms.Add(bm);
+                else this.bms[2] = bm;
+            }
+        }
+
+
+
+        private void mergeBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.result = combine(this.bms);
+                pictureBoxResult.Image = this.result;
+                resizePictureBox(pictureBoxResult, this.result);
+            }
+            catch (Exception ex)
+            {
+                errorText(ex.Message);
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            this.bms.Clear();
+            pictureBox1.Image = null;
+            pictureBox2.Image = null;
+            pictureBox3.Image = null;
+
+            pictureBoxResult.Image = null;
+            pictureBoxResult.Height = 158;
+            pictureBoxResult.Width =  518;
+
+            labelError.Visible = false;
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            saveImage(this.result);
+        }
+
+
+        private void testButton_Click(object sender, EventArgs e)
+        {
+            testLabel.Text = this.bms.Count().ToString();
+        }
+
+
+        ///////////////////////////////////////////////////
+
+        private void errorText(string text = "Erreur")
+        {
+            labelError.Visible = true;
+            labelError.Text = text;
+        }
+
+        private void resizePictureBox(PictureBox pb, Bitmap bm)
+        {
+            pb.Height = bm.Height / 8;
+            pb.Width = bm.Width / 8;
+        }
+
+        private static Bitmap combine(List<Bitmap> sources)
+        {
+            List<int> imageHeights = new List<int>();
+            List<int> imageWidths = new List<int>();
+
+            foreach(Bitmap img in sources)
+            {
+                imageHeights.Add(img.Height);
+                imageWidths.Add(img.Width);
+            }
+
+            Bitmap result = new Bitmap(imageWidths.Sum(), imageHeights.Max());
+            
+            using (Graphics g = Graphics.FromImage(result))
+            {
+                int width = 0;
+                for(int i=0; i<sources.Count; i++)
+                {
+                    if(i==0) g.DrawImage(sources[i], 0, 0);
+                    else g.DrawImage(sources[i], width, 0);
+                    width += sources[i].Width;
+                }
+            }
+            return result;
+        }
+
+        private Bitmap openImage(PictureBox pb)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = this.filter;
+
+            Bitmap bm = null;
+            if (ofd.ShowDialog() == DialogResult.OK) bm = new Bitmap(ofd.OpenFile());   
+            pb.Image = bm;
+            
+            return bm;
+        }
+
+        private void saveImage(Image img)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = this.filter;
+            sfd.FileName = "combinedImage";
+
+            if(sfd.ShowDialog() == DialogResult.OK) img.Save(sfd.FileName);
+        }
+
+        private void pictureBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            var data = e.Data.GetData(DataFormats.FileDrop);
+            if (data != null)
+            {
+                var filename = data as string[];
+                if (filename.Length > 0)
+                {
+
+                }
+            }
+        }
+    }
+}
